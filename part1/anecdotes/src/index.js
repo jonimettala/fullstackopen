824a7513f9e03b2ab0f96238e45acbd37010d975
Joni Mettälä <joni.mettala@gmail.com>
2021-01-03 19:03:09 +0200

    Add step 3 changes

--- part1/anecdotes/src/index.js
+++ part1/anecdotes/src/index.js
@@ -10,19 +10,30 @@ const Button = (props) => (
 const App = (props) => {
   const [selected, setSelected] = useState(0)
   const [points, setPoints] = useState(new Array(6).fill(0))
+  const [largest, setLargest] = useState({ index: 0, votes: 0})
   
   const vote = (selected) => {
       const newPoints = [...points]
       newPoints[selected] += 1
+      
+      if (newPoints[selected] > largest.votes) {
+          setLargest({ index: selected, votes: newPoints[selected] })
+          console.log(largest)
+      }
+
       setPoints(newPoints)
   }
 
   return (
     <div>
+      <h1>Anecdote of the day</h1>
       {props.anecdotes[selected]}<br />
       {`has ${points[selected]} votes`}<br />
       <Button handleClick={() => vote(selected)} text='vote' />
       <Button handleClick={() => setSelected(Math.floor(Math.random() * 6))} text='next anecdote' />
+      <h1>Anecdote with most votes</h1>
+      {props.anecdotes[largest.index]}<br />
+      {`has ${largest.votes} votes`}
     </div>
   )
 }
