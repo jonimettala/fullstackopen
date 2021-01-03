d32296c337b4ba67ab14f4a6f456b758663f6898
Joni Mettälä <joni.mettala@gmail.com>
2021-01-03 17:20:17 +0200

    Add unicafe step 1 changes

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -1,17 +1,46 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React, { useState } from 'react'
+import ReactDOM from 'react-dom'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+const Button = (props) => (
+  <button onClick={props.handleClick}>
+    {props.text}
+  </button>
+)
+
+const App = () => {
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  const setToGood = (newGood) => {
+    setGood(newGood)
+  }
+
+  const setToNeutral = (newNeutral) => {
+    setNeutral(newNeutral)
+  }
+
+  const setToBad = (newBad) => {
+    setBad(newBad)
+  }
+
+  return (
+    <div>
+      <h1>give feedback</h1>
+      <Button text="good" handleClick={() => setToGood(good + 1)} />
+      <Button text="neutral" handleClick={() => setToNeutral(neutral + 1)} />
+      <Button text="bad" handleClick={() => setToBad(bad + 1)} />
+      <h1>statistics</h1>
+      <p>
+        good {good}<br />
+        neutral {neutral}<br />
+        bad {bad}
+      </p>
+    </div>
+  )
+}
+
+ReactDOM.render(<App />, 
   document.getElementById('root')
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+)
