e9fec7def2ce458f88df82e74aa208bb3e5ef3a6
Joni Mettälä <joni.mettala@gmail.com>
2021-01-03 17:35:17 +0200

    Add unicafe step 2 changes

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -12,30 +12,40 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [all, setAll] = useState(0)
+  const [score, setScore] = useState(0)
 
   const setToGood = (newGood) => {
     setGood(newGood)
+    setScore(score + 1)
+    setAll(all + 1)
   }
 
   const setToNeutral = (newNeutral) => {
     setNeutral(newNeutral)
+    setAll(all + 1)
   }
 
   const setToBad = (newBad) => {
     setBad(newBad)
+    setScore(score - 1)
+    setAll(all + 1)
   }
 
   return (
     <div>
       <h1>give feedback</h1>
-      <Button text="good" handleClick={() => setToGood(good + 1)} />
-      <Button text="neutral" handleClick={() => setToNeutral(neutral + 1)} />
-      <Button text="bad" handleClick={() => setToBad(bad + 1)} />
+      <Button text='good' handleClick={() => setToGood(good + 1)} />
+      <Button text='neutral' handleClick={() => setToNeutral(neutral + 1)} />
+      <Button text='bad' handleClick={() => setToBad(bad + 1)} />
       <h1>statistics</h1>
       <p>
         good {good}<br />
         neutral {neutral}<br />
-        bad {bad}
+        bad {bad}<br />
+        all {all}<br />
+        average {all > 0 ? score / all : 'N/A'}<br />
+        positive {all > 0 ? `${100 * good / all} %` : `N/A`}
       </p>
     </div>
   )
