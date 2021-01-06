cf78b4ec8c4fec51e8725e61bba43141e28563ca
Joni Mettälä <joni.mettala@gmail.com>
2021-01-06 17:49:17 +0200

    Implement exercise 2.11

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,16 +1,12 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
 
 import Filter from './Filter'
 import PersonForm from './PersonForm'
 import Persons from './Persons'
 
 const App = () => {
-  const [ persons, setPersons ] = useState([
-    { name: 'Arto Hellas', number: '040-123456' },
-    { name: 'Ada Lovelace', number: '39-44-5323523' },
-    { name: 'Dan Abramov', number: '12-43-234345' },
-    { name: 'Mary Poppendieck', number: '39-23-6423122' }
-  ]) 
+  const [ persons, setPersons ] = useState([]) 
   const [ newName, setNewName ] = useState('')
   const [ newNumber, setNewNumber ] = useState('')
   const [ filter, setFilter ] = useState('')
@@ -26,6 +22,14 @@ const App = () => {
   const handleFilterChange = (e) => {
     setFilter(e.target.value)
   }
+  const fetchPersons = () => {
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        setPersons(response.data)
+      })
+  }
+  useEffect(fetchPersons, [])
 
   const addPerson = (e) => {
     e.preventDefault()
--- part2/phonebook/src/PersonForm.js
+++ part2/phonebook/src/PersonForm.js
@@ -1,5 +1,3 @@
-import { createContext } from "react";
-
 import React from 'react'
 
 const PersonForm = ({ addPerson, newName, handleNameChange, newNumber, handleNumberChange }) => {
