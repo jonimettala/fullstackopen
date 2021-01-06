f1c8cb55df3c5853f1b7790cdec1c7ffbaa5e2a1
Joni Mettälä <joni.mettala@gmail.com>
2021-01-06 18:33:37 +0200

    Implement 2.12

--- part2/countries/src/App.js
+++ part2/countries/src/App.js
@@ -1,8 +1,33 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
+
+import CountryList from './CountryList'
 
 const App = () => {
+
+  const [ countries, setCountries ] = useState([])
+  const [ filter, setFilter ] = useState('')
+
+  const handleFilterChange = (e) => {
+    setFilter(e.target.value)
+  }
+
+  const fetchCountries = () => {
+    axios
+      .get('https://restcountries.eu/rest/v2/all')
+      .then(response => {
+        setCountries(response.data)
+      })
+  }
+  useEffect(fetchCountries, [])
+
+  const countriesToShow = countries.filter((country) => country.name.toLowerCase().includes(filter))
+
   return (
-    <h1>countries</h1>
+    <>
+      filter shown with <input value={filter} onChange={handleFilterChange} /><br />
+      <CountryList countries={countriesToShow} />
+    </>
   )
 }
 
---
+++ part2/countries/src/CountryList.js
@@ -0,0 +1,35 @@
+import React from 'react'
+
+const CountryList = ({ countries }) => {
+  console.log(countries)
+  if (countries.length === 0) {
+    return <p>No matches, specify another filter</p>
+  } else if (countries.length <= 10 && countries.length > 1) {
+    return (
+      <ul>
+        {countries.map((country) => <li key={country.name}>{country.name}</li>)}<br />
+      </ul>
+    )
+  } else if (countries.length > 10) {
+    return <p>Too many matches, specify another filter</p>
+  } else {
+    const country = countries[0]
+
+    return (
+      <>
+        <h1>{country.name}</h1>
+        <p>
+          capital {country.capital}<br />
+          population {country.population}
+        </p>
+        <h2>Languages</h2>
+        <ul>
+          {country.languages.map((language) => <li key={language.name}>{language.name}</li>)}
+        </ul>
+        <img src={country.flag} alt={country.name} width='150' />
+      </>
+    )
+  }
+}
+
+export default CountryList
